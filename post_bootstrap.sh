3cdba2b7e457de921418cd4ccc3d821a253f23f9
Mendel van 't Riet <mendel@vantriet.nl>
2016-06-30 16:30:31 +0200

    moved java to ansible

--- post_bootstrap.sh
+++ post_bootstrap.sh
@@ -1,9 +1,6 @@
 # source me
 
-SCALA_VERSION=2.10.3
 SPARK_VERSION=1.5.2
-MAVEN_VERSION=3.3.3
-SBT_VERSION=0.13
 SHELL_RC=~/.zshrc
 
 function addToPath {
@@ -25,32 +22,6 @@ if [[ -f "/vagrant/spark-$SPARK_VERSION-bin-hadoop2.6.tgz" ]]; then
 else
 	echo "[WARNING] Spark not installed"
 fi
-
-# maven
-if [[ -f "/vagrant/apache-maven-$MAVEN_VERSION-bin.tar.gz" ]]; then
-	echo "[INFO] Installing Maven..."
-	tar -xzf /vagrant/apache-maven-$MAVEN_VERSION-bin.tar.gz -C ~/applications/
-	ln -s apache-maven-$MAVEN_VERSION ~/applications/maven
-	ln -s ../applications/maven/bin/mvn ~/bin/mvn
-	#cp /vagrant/setings.xml ~/.m2
-else
-	echo "[WARNING] Maven not installed"
-fi
-
-# eclipse
-if [[ -f "/vagrant/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz" ]]; then
-	echo "[INFO] Installing Eclipse..."
-	tar -xzf /vagrant/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz -C ~/applications/
-	ln -s ../applications/eclipse/eclipse ~/bin/eclipse
-#	~/bin/eclipse \
-#		-application org.eclipse.equinox.p2.director \
-#		-repository http://download.eclipse.org/egit/updates/ \
-#		-installIU org.eclipse.egit \
-#		-destination ~/applications/eclipse/
-else
-	echo "[WARNING] Eclipse not installed"
-fi
-
 
 echo "Done"
 
